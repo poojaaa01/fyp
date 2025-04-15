4cc603a0e3ffd15191edf071fa9478b89ba4661f
Pooja <ppooja21@tbc.edu.np>
2025-04-15 19:21:07 +0545

    Created Doctor Category

---
+++ lib/models/doc_type.dart
@@ -0,0 +1,18 @@
+class DoctorType {
+  final String docId,
+      docTitle,
+      docPrice,
+      docCategory,
+      docDescription,
+      docImage,
+      docQuantity;
+
+  DoctorType({
+    required this.docId,
+    required this.docTitle,
+    required this.docPrice,
+    required this.docCategory,
+    required this.docDescription,
+    required this.docImage,
+    required this.docQuantity,});
+  }
