99fa374cd744d625e8f545b7c6d46e02a78275f6
Pooja <ppooja21@tbc.edu.np>
2025-04-22 22:26:17 +0545

    Allows the user to upload an image

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,17 +1,21 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:fyp/consts/validator.dart';
-import 'package:fyp/screens/loading_manager.dart';
+import 'package:fyp/root_screen.dart';
 import 'package:fyp/services/app_functions.dart';
 import 'package:fyp/widgets/app_name_text.dart';
 import 'package:fyp/widgets/subtitle_text.dart';
 import 'package:fyp/widgets/title_text.dart';
 import 'package:image_picker/image_picker.dart';
-import '../../root_screen.dart';
+
 import '../../widgets/auth/image_picker_widget.dart';
+import '../loading_manager.dart';
 
 class RegisterScreen extends StatefulWidget {
   static const routName = "/RegisterScreen";
@@ -38,6 +42,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   XFile? _pickedImage;
   bool _isLoading = false;
   final auth = FirebaseAuth.instance;
+  String? userImageUrl;
 
   @override
   void initState() {
@@ -73,6 +78,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
     final isValid = _formkey.currentState!.validate();
     FocusScope.of(context).unfocus();
 
+    if(_pickedImage == null) {
+      AppFunctions.showErrorOrWarningDialog(context: context, subtitle: "Choose your image", fct: (){});
+      return;
+    }
     if (isValid) {
       try {
         setState(() {
@@ -83,37 +92,33 @@ class _RegisterScreenState extends State<RegisterScreen> {
           email: _emailController.text.trim(),
           password: _passwordController.text.trim(),
         );
-
-        await auth.currentUser?.reload(); // 🔄 Ensures user info is up to date
         final User? user = auth.currentUser;
+        final String uid = user!.uid;
+        final ref = FirebaseStorage.instance.ref().child("usersImages").child("${_emailController.text.trim()}.jpg");
+        await ref.putFile(File(_pickedImage!.path));
+        userImageUrl = await ref.getDownloadURL();
 
-        if (user != null) {
-          final String uid = user.uid;
-
-          await FirebaseFirestore.instance.collection("users").doc(uid).set({
-            'userId': uid,
-            'userName': _nameController.text,
-            'userImage': "",
-            'userEmail': _emailController.text.toLowerCase(),
-            'createdAt': Timestamp.now(),
-            'userAppointment': [],
-          });
-          print("WRITING TO FIRESTORE...");
-
-          Fluttertoast.showToast(
-            msg: "An account has been created",
-            textColor: Colors.white,
-          );
-          if (!mounted) return;
-          Navigator.pushReplacementNamed(context, RootScreen.routeName);
-        }
-      } on FirebaseException catch (error) {
+        await FirebaseFirestore.instance.collection("users").doc(uid).set({
+          'userId': uid,
+          'userName': _nameController.text,
+          'userImage': userImageUrl,
+          'userEmail': _emailController.text.toLowerCase(),
+          'createdAt': Timestamp.now(),
+          'userAppointment': [],
+        });
+        Fluttertoast.showToast(
+          msg: "An account has been created",
+          textColor: Colors.white,
+        );
+        if (!mounted) return;
+        Navigator.pushReplacementNamed(context, RootScreen.routeName);
+      }on FirebaseException catch (error) {
         await AppFunctions.showErrorOrWarningDialog(
           context: context,
           subtitle: error.message.toString(),
           fct: () {},
         );
-      } catch (error) {
+      }catch(error){
         await AppFunctions.showErrorOrWarningDialog(
           context: context,
           subtitle: error.toString(),
@@ -127,31 +132,29 @@ class _RegisterScreenState extends State<RegisterScreen> {
     }
   }
 
-
   Future<void> localImagePicker() async {
     final ImagePicker imagePicker = ImagePicker();
     await AppFunctions.imagePickerDialog(
       context: context,
       cameraFCT: () async {
         _pickedImage = await imagePicker.pickImage(source: ImageSource.camera);
-        setState(() {
-        });
+        setState(() {});
       },
       galleryFCT: () async {
         _pickedImage = await imagePicker.pickImage(source: ImageSource.gallery);
+        setState(() {});
+      },
+      removeFCT: () {
         setState(() {
+          _pickedImage = null;
         });
       },
-      removeFCT:() {setState(() {
-        _pickedImage = null;
-      });}
     );
   }
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -165,28 +168,23 @@ class _RegisterScreenState extends State<RegisterScreen> {
               child: Column(
                 children: [
                   // const BackButton(),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  const AppNameTextWidget(
-                    fontSize: 30,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
+                  const SizedBox(height: 60),
+                  const AppNameTextWidget(fontSize: 30),
+                  const SizedBox(height: 30),
                   const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          TitlesTextWidget(label: "Welcome back!"),
-                          SubtitleTextWidget(
-                              label: "Sign up to begin your journey to a calmer mind."),
-                        ],
-                      )),
-                  const SizedBox(
-                    height: 30,
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TitlesTextWidget(label: "Welcome back!"),
+                        SubtitleTextWidget(
+                          label:
+                          "Sign up to begin your journey to a calmer mind.",
+                        ),
+                      ],
+                    ),
                   ),
+                  const SizedBox(height: 30),
                   SizedBox(
                     height: size.width * 0.3,
                     width: size.width * 0.3,
@@ -197,9 +195,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       },
                     ),
                   ),
-                  const SizedBox(
-                    height: 30,
-                  ),
+                  const SizedBox(height: 30),
                   Form(
                     key: _formkey,
                     child: Column(
@@ -212,9 +208,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           keyboardType: TextInputType.name,
                           decoration: const InputDecoration(
                             hintText: 'Full Name',
-                            prefixIcon: Icon(
-                              Icons.person,
-                            ),
+                            prefixIcon: Icon(Icons.person),
                           ),
                           onFieldSubmitted: (value) {
                             FocusScope.of(context).requestFocus(_emailFocusNode);
@@ -223,9 +217,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             return Validators.displayNamevalidator(value);
                           },
                         ),
-                        const SizedBox(
-                          height: 16.0,
-                        ),
+                        const SizedBox(height: 16.0),
                         TextFormField(
                           controller: _emailController,
                           focusNode: _emailFocusNode,
@@ -233,21 +225,18 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           keyboardType: TextInputType.emailAddress,
                           decoration: const InputDecoration(
                             hintText: "Email address",
-                            prefixIcon: Icon(
-                              IconlyLight.message,
-                            ),
+                            prefixIcon: Icon(IconlyLight.message),
                           ),
                           onFieldSubmitted: (value) {
-                            FocusScope.of(context)
-                                .requestFocus(_passwordFocusNode);
+                            FocusScope.of(
+                              context,
+                            ).requestFocus(_passwordFocusNode);
                           },
                           validator: (value) {
                             return Validators.emailValidator(value);
                           },
                         ),
-                        const SizedBox(
-                          height: 16.0,
-                        ),
+                        const SizedBox(height: 16.0),
                         TextFormField(
                           controller: _passwordController,
                           focusNode: _passwordFocusNode,
@@ -256,9 +245,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           obscureText: obscureText,
                           decoration: InputDecoration(
                             hintText: "Password",
-                            prefixIcon: const Icon(
-                              IconlyLight.lock,
-                            ),
+                            prefixIcon: const Icon(IconlyLight.lock),
                             suffixIcon: IconButton(
                               onPressed: () {
                                 setState(() {
@@ -273,16 +260,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             ),
                           ),
                           onFieldSubmitted: (value) async {
-                            FocusScope.of(context)
-                                .requestFocus(_repeatPasswordFocusNode);
+                            FocusScope.of(
+                              context,
+                            ).requestFocus(_repeatPasswordFocusNode);
                           },
                           validator: (value) {
                             return Validators.passwordValidator(value);
                           },
                         ),
-                        const SizedBox(
-                          height: 16.0,
-                        ),
+                        const SizedBox(height: 16.0),
                         TextFormField(
                           controller: _repeatPasswordController,
                           focusNode: _repeatPasswordFocusNode,
@@ -291,9 +277,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           obscureText: obscureText,
                           decoration: InputDecoration(
                             hintText: "Repeat password",
-                            prefixIcon: const Icon(
-                              IconlyLight.lock,
-                            ),
+                            prefixIcon: const Icon(IconlyLight.lock),
                             suffixIcon: IconButton(
                               onPressed: () {
                                 setState(() {
@@ -317,9 +301,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             );
                           },
                         ),
-                        const SizedBox(
-                          height: 36.0,
-                        ),
+                        const SizedBox(height: 36.0),
                         SizedBox(
                           width: double.infinity,
                           child: ElevatedButton.icon(
@@ -327,9 +309,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                               padding: const EdgeInsets.all(12.0),
                               // backgroundColor: Colors.red,
                               shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                  12.0,
-                                ),
+                                borderRadius: BorderRadius.circular(12.0),
                               ),
                             ),
                             icon: const Icon(IconlyLight.addUser),
